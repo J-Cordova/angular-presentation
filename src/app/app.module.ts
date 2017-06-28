a351904ebaf83452ebc065c652929b294b6596e4
Cordova <jordan.cordova@tylertech.com>
2017-06-28 14:38:53 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {FlexLayoutModule} from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import {DialogComponent} from './dialog/dialog.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
