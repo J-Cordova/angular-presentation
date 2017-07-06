a83f14dba8126788ef781cb5bf9ceb0ef7edea9d
J-Cordova <jordan.m.cordova@gmail.com>
2017-07-05 21:46:34 -0400

    Final

--- src/app/animal-sidenav/animal-sidenav.component.ts
+++ src/app/animal-sidenav/animal-sidenav.component.ts
@@ -6,9 +6,21 @@ import { Animal } from '../shared/animal.model';
 import { AnimalService } from '../shared/animal.service';
 import { AnimalSelectedService } from '../shared/animal-selected.service';
 
+
+
 @Component({
   selector: 'animal-sidenav',
-  templateUrl: './animal-sidenav.html',
+  //templateUrl: './animal-sidenav.html',
+  template: `    
+  <md-sidenav mode="side" opened>
+      <md-nav-list>
+        <h2 style="text-align:center;">Animals</h2>
+        <md-list-item  *ngFor="let animal of animals" (click)="onAnimalSelected(animal)">        
+           <md-icon svgIcon="{{animal.Avatar}}" class="avatar"></md-icon>
+          <h3 style="padding-left: 1em">{{animal.Name}}</h3>
+        </md-list-item>
+      </md-nav-list>
+    </md-sidenav> `,
   styleUrls: ['./animal-sidenav.css'],
 })
 export class AnimalSideNavComponent
--- src/app/shared/animal-selected.service.ts
+++ src/app/shared/animal-selected.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Animal } from './animal.model';
 import { AnimalType } from './animaltype.enum';
-import {Observable} from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class AnimalSelectedService {
